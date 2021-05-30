db77851f1773819d532f3ebd975b2418714fe157
Natalia Sorokina <pticasonabird@gmail.com>
2021-05-30 18:38:52 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+};
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    refs.input.value === "" ? refs.span.textContent = 'незнакомец' : refs.span.textContent = event.currentTarget.value;
+};
+
+// refs.input.addEventListener('input', event => {
+//     refs.input.value === "" ? refs.span.textContent = 'незнакомец' : refs.span.textContent = event.currentTarget.value;
+// });
